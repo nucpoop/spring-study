9e1f1db1ad0da065cc293e7a29a8ec4994eb2c35
nucpoop <kjm5546@gmail.com>
2022-10-17 09:46:05 +0900

    first commit

---
+++ src/main/java/com/nucpoop/server/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.nucpoop.server.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    
+    @GetMapping("/health-check")
+    public ResponseEntity<String> healthCheck(){
+        return ResponseEntity.ok("hello");
+    }
+}
