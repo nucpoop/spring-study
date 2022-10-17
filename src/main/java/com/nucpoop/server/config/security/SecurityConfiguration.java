99244137316431ed66ca3eb3452c4ebc97a97ad4
nucpoop <kjm5546@gmail.com>
2022-10-17 14:24:02 +0900

    security config 설정

---
+++ src/main/java/com/nucpoop/server/config/security/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.nucpoop.server.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/health-check").permitAll();
+            //.anyRequest().hasRole("USER")
+            //.and()
+            //.addFilterBefore()
+        return http.build();
+    }
+
+}
--- src/main/java/com/nucpoop/server/controller/IndexController.java
+++ src/main/java/com/nucpoop/server/controller/IndexController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    
+
     @GetMapping("/health-check")
-    public ResponseEntity<String> healthCheck(){
+    public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("hello");
     }
 }
